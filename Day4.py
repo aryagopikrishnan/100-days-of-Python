639128e0a943dac1b5b0f29a9fadd9fe7e1e802e
Arya <aryagxr@gmail.com>
2023-07-12 19:22:38 -0400

    Day 4

---
+++ Day4.py
@@ -0,0 +1,12 @@
+#Day 4 - Randomization
+
+import random
+
+coin_side = random.randint(0,1)
+if coin_side == 1:
+    print("Heads")
+else:
+    print("Tails")
+
+#-------------------------------------------------------
+# Code 2: 
