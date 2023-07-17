441ccf58914d37b5808b62dd8e555c57bd96f4bc
Arya <aryagxr@gmail.com>
2023-07-17 13:56:05 -0400

    Modified day7

--- day7.py
+++ day7.py
@@ -1,5 +1,64 @@
 # Hangman Game
 
+stages = ['''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
+
+
+
 import random
 
 word_list = ["aardvark", "baboon", "camel"]
@@ -9,21 +68,42 @@ chosen_word = random.choice(word_list)
 print(f"the word is {chosen_word}")
 display = []
 
+
 num_letters = len(chosen_word)
 for i in range(num_letters):
     display.append("_")
 print(display)
 
-guess = input("Guess a letter: ").lower()
+
+def game():
+    num_lives = 6
+    while num_lives > 0:
 
 
-for position in range(len(chosen_word)):
-    letter = chosen_word[position]
-    if letter == guess:
-        display[position] = letter
-        
+        guess = input("Guess a letter: ").lower()
 
-print(display)
+
+        for position in range(len(chosen_word)):
+            letter = chosen_word[position]
+            if letter == guess:
+                display[position] = letter
+            
+        num_lives = num_lives - 1        
+        print(display)
+        print(f"Lives left: {num_lives}")
+
+game()
+
+if "_" in display:
+    print("You Lose.")
+else: 
+    print("You Win!")  
+            
+
+
+
+
+
 
 
 
