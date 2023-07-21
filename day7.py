2638c45ab427a179cf96ade81868efca9108009b
Arya <aryagxr@gmail.com>
2023-07-20 22:10:40 -0400

    Hangman Changes

--- day7.py
+++ day7.py
@@ -76,28 +76,32 @@ print(display)
 
 
 def game():
-    num_lives = 6
-    while num_lives > 0:
+    
+        
+        num_lives = 6
+        while num_lives > 0:
 
 
-        guess = input("Guess a letter: ").lower()
+            guess = input("Guess a letter: ").lower()
 
 
-        for position in range(len(chosen_word)):
-            letter = chosen_word[position]
-            if letter == guess:
-                display[position] = letter
+            for position in range(len(chosen_word)):
+                letter = chosen_word[position]
+                if letter == guess:
+                    display[position] = letter
+                
+            if letter != guess:
+                num_lives -=1
+                      
+            print(display)
+            print(f"Lives left: {num_lives}")
+            if "_" not in display:
+                print("You Win.")
+                
             
-        num_lives = num_lives - 1        
-        print(display)
-        print(f"Lives left: {num_lives}")
-
 game()
 
-if "_" in display:
-    print("You Lose.")
-else: 
-    print("You Win!")  
+  
             
 
 
