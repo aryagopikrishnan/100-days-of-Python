900155ee311a2dcf8afdce87f327d37b5a5b6f57
Arya <aryagxr@gmail.com>
2023-07-25 22:17:38 -0400

    Dictionaries

--- day9.py
+++ day9.py
@@ -23,3 +23,27 @@ print(student_grades)
 
 #-------------------------------------------------------------------
 
+travel_log = [
+{
+  "country": "France",
+  "visits": 12,
+  "cities": ["Paris", "Lille", "Dijon"]
+},
+{
+  "country": "Germany",
+  "visits": 5,
+  "cities": ["Berlin", "Hamburg", "Stuttgart"]
+},
+]
+
+
+def add_new_country(country, num_visits, cities):
+    travel_log.append({"country" : country,"visits" : num_visits, "cities":cities})
+
+
+add_new_country("Russia", 2, ["Moscow", "Saint Petersburg"])
+print(travel_log)
+
+
+#-------------------------------------------------------------------------------------
+
