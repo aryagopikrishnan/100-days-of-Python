6945484775f4d2ea561de7dc59d6bfd28ba68c3d
Arya <aryagxr@gmail.com>
2023-07-31 22:06:10 -0400

    Framework of the code

--- day11.py
+++ day11.py
@@ -1,1 +1,25 @@
-#Capstone project
+# Blackjack Capstone project
+
+############### Our Blackjack House Rules #####################
+
+## The deck is unlimited in size. 
+## There are no jokers. 
+## The Jack/Queen/King all count as 10.
+## The the Ace can count as 11 or 1.
+
+cards = [11,2,3,4,5,6,7,8,9,10,10,10,10]
+
+## The cards in the list have equal probability of being drawn.
+## Cards are not removed from the deck as they are drawn.
+## The computer is the dealer.
+
+#START 
+
+
+# USER AND COMPUTER GETS 2 RANDOM CARDS EACH
+
+
+# USER AND COMPUETR CARD NUMBERS TO BE ADDED UP
+
+
+# IF STATEMENT: 
