6dbd7b05c7bbda03b943461f94aee47fb50da504
Arya <aryagxr@gmail.com>
2023-08-01 22:43:44 -0400

    Creating function

--- day11.py
+++ day11.py
@@ -7,7 +7,7 @@
 ## The Jack/Queen/King all count as 10.
 ## The the Ace can count as 11 or 1.
 
-cards = [11,2,3,4,5,6,7,8,9,10,10,10,10]
+
 
 ## The cards in the list have equal probability of being drawn.
 ## Cards are not removed from the deck as they are drawn.
@@ -17,9 +17,13 @@ cards = [11,2,3,4,5,6,7,8,9,10,10,10,10]
 
 
 # USER AND COMPUTER GETS 2 RANDOM CARDS EACH
-
-
 # USER AND COMPUETR CARD NUMBERS TO BE ADDED UP
-
-
 # IF STATEMENT: 
+
+import random
+
+def card_deal():
+    cards = [11,2,3,4,5,6,7,8,9,10,10,10,10]
+    random_card = random.choice(cards)
+    return random_card
+
