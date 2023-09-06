f6a405005490341d514222313c7d136e84d3447c
Arya <aryagxr@gmail.com>
2023-09-06 07:23:18 -0400

    BlackJack

--- day11.py
+++ day11.py
@@ -46,17 +46,46 @@ for _ in range(0,2):
     user_card.append(card_deal())
     computer_card.append(card_deal())
 
+while not game_over:
 
-user_score = calculate_score(user_card)
-computer_score = calculate_score(computer_card)
-
-
-print(f"Your cards: {user_card}, current score: {user_score}")
-print(f"Computer's first card: {computer_card[0]}")
-if user_score==0 or computer_score==0 or user_score>21:
-    game_over=True
+    user_score = calculate_score(user_card)
+    computer_score = calculate_score(computer_card)
 
 
+    print(f"Your cards: {user_card}, current score: {user_score}")
+    print(f"Computer's first card: {computer_card[0]}")
+    if user_score==0 or computer_score==0 or user_score>21:
+        game_over=True
 
 
+
+    else:
+        user_continue = input("Type 'y' to get another card, type 'n' to pass: ")
+        if user_continue == 'y':
+            user_card.append(card_deal())
+            user_score = calculate_score(user_card)
+            print(f"Your cards: {user_card}, current score: {user_score}")
+            print(f"Computer's first card: {computer_card[0]}")
+        else:
+            game_over=True
+
+while computer_score !=0 and computer_score<17:
+    computer_card.append(card_deal())
+    computer_score = calculate_score(computer_card)
+
+def compare(user_score, computer_score):
+    if user_score==computer_score:
+        return "Draw"
+    elif computer_score==0:
+        return "Lose, opponent has Blackjack"
+    elif user_score==0:
+        return "Win with a Blackjack"
+    elif user_score>21:
+        return "You went over. You lose"
+    elif computer_score>21:
+        return "Opponent went over. You win"
+    elif user_score>computer_score:
+        return "You win"
+    else:
+        return "You lose"   
 
